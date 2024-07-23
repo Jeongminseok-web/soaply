c78f130a7b2d7801c05d665736f076f263963f06
jeongminseok <jeong010328@gmail.com>
2024-07-23 18:14:45 +0900

    first commit

--- js/request.js
+++ js/request.js
@@ -12,7 +12,7 @@ const getData = async () => {
       let dataElement;
 
       data.map((item) => {
-        console.log(item);
+        // console.log(item);
         dataElement = `
           <div class="swiper-slide">
             <div class="slider-image">
@@ -21,7 +21,7 @@ const getData = async () => {
             <div class="slider-text">
               <h4>${item.pro_name}</h4>
               <p>${item.pro_desc}</p>
-              <a href="#" class="common-btn">자세히 보기</a>
+              <a href="details.html?idx=${item.pro_idx}" class="common-btn">자세히 보기</a>
             </div>
           </div>
         `;
